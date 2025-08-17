dc545fe19b55fe21e7a4c4fd31aeb8aef7225889
CaoMeiYouRen <caomeiyouren@qq.com>
2025-08-17 17:44:29 +0800

    chore: 移除旧的 ESLint 配置并添加新的配置文件

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+// eslint.config.js
+import cmyrConfig from 'eslint-config-cmyr/vue'
+import { defineConfig } from 'eslint/config'
+export default defineConfig([
+    cmyrConfig,
+    {
+        ignores: [
+            'node_modules',
+            'dist',
+            'public',
+            '.nuxt/**',
+            '.nuxt',
+            'coverage',
+            '.output',
+            'build',
+        ],
+    },
+    {
+        rules: {
+            'vue/multi-word-component-names': 0,
+            'no-undef': 0
+        }
+    }
+])
